e62cba825892da9d0b4d2ac691a6f9fd0c6da7cd
Chow Jia Ying <chowjiaying211@gmail.com>
2020-12-18 14:02:55 +0800

    Add an attribution to the colour syntax plugin

--- src/index.js
+++ src/index.js
@@ -194,6 +194,7 @@ function applyFontSizeHTMLTag(text, fontSize) {
 
 /**
  * Font size plugin
+ * Some code was modified from https://github.com/nhn/tui.editor/blob/master/plugins/color-syntax/src/js/index.js to learn how to make use of the TUI Editor's API to create the plugin
  * @param {Editor|Viewer} editor - instance of Editor or Viewer
  */
 
